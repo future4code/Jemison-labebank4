50fe57b3ab892ed559115fd3299f494d3252a3b6
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2022-11-15 14:32:56 -0300

    Feat: se aplicaod esse comit add type e data

---
+++ src/data.ts
@@ -0,0 +1,32 @@
+import {User,extract} from "./type"
+
+export const userAccounts:User[] = [
+    {
+        name: "Daniel Sousa",
+        CPF: "121235686525",
+        birthDate:"06-09-1999",
+        balance:10000000,
+        extract:[
+            {value:5000 , date:'20-05-2000' , description: "SSD para o Pc" }
+        ]
+    },
+    {
+        name: "Douglas Silva",
+        CPF: "121235686525",
+        birthDate:"06-01-1650",
+        balance:10000000,
+        extract:[
+            {value:5000 , date:'20-05-2000' , description: "Fone novo" }
+        ]
+    },
+    {
+        name: "Matheus Sousa",
+        CPF: "121235686525",
+        birthDate:"06-09-1500",
+        balance:10000000,
+        extract:[
+            {value:5000 , date:'20-05-1500' , description: "Compra de produtos para o barco, para a expedição no recente descobrimento do Brasil" }
+        ]
+    },
+
+]
